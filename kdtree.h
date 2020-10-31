915525042854eb7c0224984304290da9f01d5132
trnciii <doityan223@gmail.com>
2020-10-31 23:12:29 +0900

    move tree construction process into struct Tree

--- kdtree.h
+++ kdtree.h
@@ -5,24 +5,64 @@
 #include <glm/glm.hpp>
 #include "print.h"
 
-struct Node{
-	glm::vec3 min;
-	glm::vec3 max;
+struct Tree{
 
-	uint32_t size;
+	struct Node{
+		glm::vec3 min;
+		glm::vec3 max;
 
-	Node(const std::vector<glm::vec3>::iterator begin,
-		const std::vector<glm::vec3>::iterator end)
-	:min(*begin), max(*begin), size(end-begin)
-	{
-		for(auto it=begin+1; it<end; it++){
-			min = glm::min(min, *it);
-			max = glm::max(max, *it);
+		uint32_t size;
+
+		Node(const std::vector<glm::vec3>::iterator begin,
+			const std::vector<glm::vec3>::iterator end)
+		:min(*begin), max(*begin), size(end-begin)
+		{
+			for(auto it=begin+1; it<end; it++){
+				min = glm::min(min, *it);
+				max = glm::max(max, *it);
+			}
 		}
+
+		int axis(){
+			glm::vec3 dim = max - min;
+			return (dim.y<dim.x && dim.z<dim.x)? 0 : ((dim.z<dim.y)? 1 : 2);
+		}
+	};
+
+	std::vector<glm::vec3> verts;
+	std::vector<Node> nodes;
+
+	void build(){
+		Node root(verts.begin(), verts.end());
+		nodes.push_back(root);
+		split(verts.begin(), verts.end(), root.axis());
 	}
 
-	int axis(){
-		glm::vec3 dim = max - min;
-		return (dim.y<dim.x && dim.z<dim.x)? 0 : ((dim.z<dim.y)? 1 : 2);
-	}
+	void split(const std::vector<glm::vec3>::iterator begin,
+		const std::vector<glm::vec3>::iterator end,
+		const int axis)
+	{
+		std::sort(begin, end, [axis](glm::vec3 a, glm::vec3 b){return a[axis] < b[axis];});	
+		std::vector<glm::vec3>::iterator mid =  begin+(end-begin)/2; // split by count
+
+		// print
+		std::cout <<"begin " <<&(*begin) <<", mid " <<&(*mid) <<", end " <<&(*end);
+		std::cout <<", " <<axis <<std::endl;
+		for(auto it=begin; it<end; it++) print(*it);
+		std::cout <<std::endl;
+
+
+		if(1 < mid-begin){
+			Node right(begin,mid);
+			nodes.push_back(right);
+			split(begin, mid, right.axis());
+		}
+
+		if(1 < end-mid){
+			Node left(mid, end);
+			nodes.push_back(left);
+			split(mid, end, left.axis());
+		}
+	};
+
 };
--- main.cpp
+++ main.cpp
@@ -6,60 +6,28 @@
 #include "Random.h"
 #include "print.h"
 
-void split(std::vector<Node>& nodes, const std::vector<glm::vec3>::iterator begin, const std::vector<glm::vec3>::iterator end, const int axis){
-	std::sort(begin, end, [axis](glm::vec3 a, glm::vec3 b){return a[axis] < b[axis];});
-	
-	std::vector<glm::vec3>::iterator mid =  begin+(end-begin)/2; // split by count
-
-	// print
-	std::cout <<"begin " <<&(*begin) <<", mid " <<&(*mid) <<", end " <<&(*end);
-	std::cout <<", " <<axis <<std::endl;
-	for(auto it=begin; it<end; it++) print(*it);
-	std::cout <<std::endl;
-
-
-	if(1 < mid-begin){
-		Node right(begin,mid);
-		
-		nodes.push_back(right);
-		split(nodes, begin, mid, right.axis());
-	}
-
-	if(1 < end-mid){
-		Node left(mid, end);
-
-		nodes.push_back(left);
-		split(nodes, mid, end, left.axis());
-	}
-
-	return;
-}
-
 int main(void){
 	RNG rng;
-	std::vector<glm::vec3> verts;
-	std::vector<Node> nodes;
+	Tree tree;
 
 	for(int i=0; i<8; i++){
 		glm::vec3 v(rng.uniform(), rng.uniform(), rng.uniform());
-		verts.push_back(v);
+		tree.verts.push_back(v);
 		print(v);
 	}
 	printRule();
 	
-	Node root(verts.begin(), verts.end());
-	nodes.push_back(root);
-	split(nodes, verts.begin(), verts.end(), root.axis());
+	tree.build();
 	printRule();
 
-	for(auto& n : nodes){
+	for(auto& n : tree.nodes){
 		std::cout <<n.size;
 		print(n.min, " | ");
 		print(n.max);
 	}
 	printRule();
 
-	for(auto& v : verts) print(v);
+	for(auto& v : tree.verts) print(v);
 	printRule();
 
 	return 0;
